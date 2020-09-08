b5ef1d1612cc102555c7803c3f5689fab4b97172
Benedicte des Arcis <bdesarcis@gmail.com>
2020-09-08 11:39:02 +0200

    add validation to url

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -6,15 +6,5 @@ class Plant < ApplicationRecord
 
   # add validation url syntax
   validates :image_url, format: { with: URI.regexp }
-
-	# verify if image url is correct
-	def image_exists?(url)
-		url = URI.parse(url)
-		http = Net::HTTP.new(url.host, url.port)
-		http.use_ssl = (url.scheme = "https")
-		req = Net::HTTP::Get.new(url.to_s)
-		res = http.request(req)
-		hash = res.each_header.to_h
-		hash["content-type"].start_with? 'image'
-	end
+  
 end
