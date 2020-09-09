8c29c2033092f4f09ea4b4dbdeb4cc86078032ed
Benedicte des Arcis <bdesarcis@gmail.com>
2020-09-09 21:42:47 +0200

    change error messgae

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,11 +1,11 @@
 class UrlValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors[attribute] << (options[:message] || "must be a valid URL") unless image_exists?(value)    
+    record.errors[attribute] << (options[:message] || "must be an image URL") unless image_exists?(value)    
   end
 
   # a URL may be technically well-formed but may 
   # not actually be valid, so this checks for both.
-  def image_exists?(url)
+def image_exists?(url)
 	url = URI.parse(url)
 	http = Net::HTTP.new(url.host, url.port)
 	http.use_ssl = (url.scheme = "https")
@@ -14,5 +14,4 @@ class UrlValidator < ActiveModel::EachValidator
 	hash = res.each_header.to_h
 	hash["content-type"].start_with? 'image'
 end
-
 end
