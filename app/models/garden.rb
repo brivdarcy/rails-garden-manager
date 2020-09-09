4c0f3840d265783438da793909a59bd4ec2db3ed
Benedicte des Arcis <bdesarcis@gmail.com>
2020-09-09 21:28:36 +0200

    add-garden-validation

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -6,5 +6,17 @@ class Garden < ApplicationRecord
 
   # add validation url syntax
   validates :banner_url, format: { with: URI.regexp }
-  
+  validates :banner_url, url: true
 end
+
+
+# verify url header contain image
+# def image_exists?(url)
+# 	url = URI.parse(url)
+# 	http = Net::HTTP.new(url.host, url.port)
+# 	http.use_ssl = (url.scheme = "https")
+# 	req = Net::HTTP::Get.new(url.to_s)
+# 	res = http.request(req)
+# 	hash = res.each_header.to_h
+# 	hash["content-type"].start_with? 'image'
+# end
